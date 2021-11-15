7989892348492c845e40b3d6d83723bdc37ef1fa
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-15 15:03:10 +0100

    Brew formula update for pipelines-as-code version 0.5.0

--- Formula/tektoncd-pac.rb
+++ Formula/tektoncd-pac.rb
@@ -5,34 +5,51 @@
 class TektoncdPac < Formula
   desc "Tekton PAC - The command line interface for interacting with Pipelines as Code"
   homepage "https://github.com/openshift-pipelines/pipelines-as-code"
-  version "0.4.4"
-  bottle :unneeded
+  version "0.5.0"
 
   on_macos do
-    url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.4.4/tkn-pac_0.4.4_MacOS_all.tar.gz"
-    sha256 "d199cfebc4a82327f9190311ff64fbf4dfdefd8d19944bfb48777220729532d4"
+    url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.5.0/tkn-pac_0.5.0_MacOS_all.tar.gz"
+    sha256 "6167eee904610900ce1153e50dcec5c26aabc2f0ebcf6f1dcda2a03f2046604a"
+
+    def install
+      bin.install "tkn-pac" => "tkn-pac"
+      output = Utils.popen_read("SHELL=bash #{bin}/tkn-pac completion bash")
+      (bash_completion/"tkn-pac").write output
+      output = Utils.popen_read("SHELL=zsh #{bin}/tkn-pac completion zsh")
+      (zsh_completion/"_tkn-pac").write output
+      prefix.install_metafiles
+    end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.4.4/tkn-pac_0.4.4_Linux_x86_64.tar.gz"
-      sha256 "75a3369c3a0a4d2fe983fb4fafee28184d7c8fe1556fdbd7c3bac976ff2bf218"
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.4.4/tkn-pac_0.4.4_Linux_arm64.tar.gz"
-      sha256 "79684d1924359d9a4018cb71c7598d9a32ddfd9bada93999868ee990b7390bea"
+      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.5.0/tkn-pac_0.5.0_Linux_arm64.tar.gz"
+      sha256 "0e9274087a429a5ccf702d81690a6c99effa13c0373241f85dc7a9871248e713"
+
+      def install
+        bin.install "tkn-pac" => "tkn-pac"
+        output = Utils.popen_read("SHELL=bash #{bin}/tkn-pac completion bash")
+        (bash_completion/"tkn-pac").write output
+        output = Utils.popen_read("SHELL=zsh #{bin}/tkn-pac completion zsh")
+        (zsh_completion/"_tkn-pac").write output
+        prefix.install_metafiles
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.5.0/tkn-pac_0.5.0_Linux_x86_64.tar.gz"
+      sha256 "9349d0af94b25bec11422c2c0ef3dff20a7a6b8f88f85948a65e50db40745b8f"
+
+      def install
+        bin.install "tkn-pac" => "tkn-pac"
+        output = Utils.popen_read("SHELL=bash #{bin}/tkn-pac completion bash")
+        (bash_completion/"tkn-pac").write output
+        output = Utils.popen_read("SHELL=zsh #{bin}/tkn-pac completion zsh")
+        (zsh_completion/"_tkn-pac").write output
+        prefix.install_metafiles
+      end
     end
   end
 
   depends_on "tektoncd-cli" => :optional
   depends_on "git"
-
-  def install
-    bin.install "tkn-pac" => "tkn-pac"
-    output = Utils.popen_read("SHELL=bash #{bin}/tkn-pac completion bash")
-    (bash_completion/"tkn-pac").write output
-    output = Utils.popen_read("SHELL=zsh #{bin}/tkn-pac completion zsh")
-    (zsh_completion/"_tkn-pac").write output
-    prefix.install_metafiles
-  end
 end
