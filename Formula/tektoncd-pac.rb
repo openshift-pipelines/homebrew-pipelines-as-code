94d02c837e5db2dfd5453e0720926d167d199c00
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-08 07:10:40 +0200

    Brew formula update for pipelines-as-code version 0.5.7

--- Formula/tektoncd-pac.rb
+++ Formula/tektoncd-pac.rb
@@ -5,11 +5,11 @@
 class TektoncdPac < Formula
   desc "Tekton PAC - The command line interface for interacting with Pipelines as Code"
   homepage "https://github.com/openshift-pipelines/pipelines-as-code"
-  version "0.5.6"
+  version "0.5.7"
 
   on_macos do
-    url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.5.6/tkn-pac_0.5.6_MacOS_all.tar.gz"
-    sha256 "61ab9741c1a5ceb782f3cddb661dbc6abcb83aea1dead0609b3ab1bf18b0830e"
+    url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.5.7/tkn-pac_0.5.7_MacOS_all.tar.gz"
+    sha256 "f5c2e91c4777254a0ff62a820332777982301f69bbb55f17a42322b9419fa1be"
 
     def install
       bin.install "tkn-pac" => "tkn-pac"
@@ -22,9 +22,9 @@ class TektoncdPac < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.5.6/tkn-pac_0.5.6_Linux_x86_64.tar.gz"
-      sha256 "767dbef4eaf7833e9d6e421ffdb84fb6e3a3e6ca7493b36c8de8368f9e386169"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.5.7/tkn-pac_0.5.7_Linux_arm64.tar.gz"
+      sha256 "8d47b7f763ba9598d77499da21783fbdf99a5255db2aa2385dcf0d682a732e80"
 
       def install
         bin.install "tkn-pac" => "tkn-pac"
@@ -35,9 +35,9 @@ class TektoncdPac < Formula
         prefix.install_metafiles
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.5.6/tkn-pac_0.5.6_Linux_arm64.tar.gz"
-      sha256 "0a72da09e1918fc7f364d4899704d315aa61d2ce3628706b37bd1d4ee85a41d8"
+    if Hardware::CPU.intel?
+      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.5.7/tkn-pac_0.5.7_Linux_x86_64.tar.gz"
+      sha256 "676798aac59e11443e2bd0487c269491314ca1b5f53a0218b23a42499590bdcd"
 
       def install
         bin.install "tkn-pac" => "tkn-pac"
