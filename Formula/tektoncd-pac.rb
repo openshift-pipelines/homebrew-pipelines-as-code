67187572208325834ca18a728d2399790db08162
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-29 14:21:37 +0200

    Brew formula update for pipelines-as-code version 0.7.0

--- Formula/tektoncd-pac.rb
+++ Formula/tektoncd-pac.rb
@@ -9,7 +9,7 @@ class TektoncdPac < Formula
 
   on_macos do
     url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.7.0/tkn-pac_0.7.0_MacOS_all.tar.gz"
-    sha256 "c15de1599c112fd2b868f051ed8497407421ddb1549ac20a6af9aba3fcce985d"
+    sha256 "56a86969c05484d14816e4ae310dd1ed3e9fb9fa16ad7c404bdd8fb717310078"
 
     def install
       bin.install "tkn-pac" => "tkn-pac"
@@ -22,9 +22,9 @@ class TektoncdPac < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.7.0/tkn-pac_0.7.0_Linux_arm64.tar.gz"
-      sha256 "1e67bba204d9c8ebb8f972421816ecef030b87fa71a9b43fb495dcd9e96d9308"
+    if Hardware::CPU.intel?
+      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.7.0/tkn-pac_0.7.0_Linux_x86_64.tar.gz"
+      sha256 "1c74c1e9b920414a301e13ab68f5fd4dda2d28caaa1cb5641f4ba6beb3141305"
 
       def install
         bin.install "tkn-pac" => "tkn-pac"
@@ -35,9 +35,9 @@ class TektoncdPac < Formula
         prefix.install_metafiles
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.7.0/tkn-pac_0.7.0_Linux_x86_64.tar.gz"
-      sha256 "7bad367234864fe5de053cd9dadd0bd5a24d978e660b85e8ff2da17246d26564"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/0.7.0/tkn-pac_0.7.0_Linux_arm64.tar.gz"
+      sha256 "0219a9bb0cefe19181652ab2e8b5e38598778f537d698aa745cdf53d718dc947"
 
       def install
         bin.install "tkn-pac" => "tkn-pac"
